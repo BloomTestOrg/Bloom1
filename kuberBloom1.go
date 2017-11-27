b1ba6fed4ce01ddb2b53e059a247bcd17d5c6ea6
cdasaroDev <cdasaroDev@gmail.com>
2017-11-27 15:59:15 -0500

    First commit

---
+++ kuberBloom1.go
@@ -0,0 +1,8 @@
+package Bloom1
+
+func AddToComplexTypeOrg() {
+
+	var description := "Build scheduling stress tests inside of kubernetes unit testing library which measure the time to schedule 1000s of pods when node matching needs to occur using complex inequality/predicate logic (i.e. memory, cpu constraints, etc.). This is mostly kubernetes coding activity."
+
+	AddToComplexTypeOrg(description);
+}
